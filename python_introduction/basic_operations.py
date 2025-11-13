72a2a54dde2de35c353e853c9ebce1de50417818
Emmanuel Samson <samsonemmanuel9025@gmail.com>
2025-11-13 20:13:40 +0100

    Adding control flow directory

--- python_introduction/basic_operations.py
+++
@@ -1,9 +0,0 @@
-number1 = 10
-number2 = 5
-result = number1 + number2
-result2 = number1 - number2
-result3 = number1 * number2
-
-print(f'Addition of {number1} and {number2} is {result}')
-print(f'Subtraction of {number2} from {number1} is {result2}')
-print(f'Multiplication of {number1} and {number2} is {result3}')
