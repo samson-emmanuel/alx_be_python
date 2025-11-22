41f4c0294d6c6e660010321c946c7e900173a50e
Emmanuel Samson <samsonemmanuel9025@gmail.com>
2025-11-22 11:42:01 +0100

    Correct file pushed for arithmetic_operation file

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -14,3 +14,12 @@ def perform_operation(num1:float, num2:float, operation:str):
     else:
         return "Invalid operation"
     return "Operation performed successfully"
+
+
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
+
+result = perform_operation(num1, num2, operation)
+print(f"Result: {result}")
