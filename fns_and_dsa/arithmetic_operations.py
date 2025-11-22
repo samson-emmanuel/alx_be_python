b81d0e5e8cd22c81fb16d89ded98c700ceee2166
Emmanuel Samson <samsonemmanuel9025@gmail.com>
2025-11-22 11:46:36 +0100

    Update file done for coditional statement

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,5 +1,7 @@
-def perform_operation(num1:float, num2:float, operation:str):
-    if operation == "subtract":
+def perform_operation(num1, num2, operation):
+    if num1 == 0 or num2 == 0:
+        print("Error: Cannot perform operations with zero.")
+    elif operation == "subtract":
         result = num1 - num2
         print(f'Result: {result}')
     elif operation == "add":
@@ -14,12 +16,3 @@ def perform_operation(num1:float, num2:float, operation:str):
     else:
         return "Invalid operation"
     return "Operation performed successfully"
-
-
-
-num1 = float(input("Enter the first number: "))
-num2 = float(input("Enter the second number: "))
-operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
-
-result = perform_operation(num1, num2, operation)
-print(f"Result: {result}")
