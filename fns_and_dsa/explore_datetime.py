8a8da6fc14c9f895b759414089347985c242e6e1
Emmanuel Samson <samsonemmanuel9025@gmail.com>
2025-11-22 20:40:56 +0100

    Corrected the date and time format

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -3,11 +3,11 @@ from datetime import datetime, timedelta
 
 def display_current_datetime():
     current_date = datetime.now()
-    print(current_date.strftime("YYYY-MM-DD HH:MM:SS"))
+    print(current_date.strftime("%Y-%m-%d %H:%M:%S"))
 
 def calculate_future_date(days_to_add):
     future_date = datetime.now() + timedelta(days=days_to_add)
-    print(future_date.strftime("YYYY-MM-DD"))
+    print(future_date.strftime("%Y-%m-%d %H:%M:%S"))
 
 
 
