7e9b0920fc7bce4e2ea3b08870108afd057e0b84
Emmanuel Samson <samsonemmanuel9025@gmail.com>
2025-11-25 15:30:11 +0100

    started bakan account file

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,15 @@
+class BankAccount:
+    def __init__(self, account_balance=0):
+        self.account_balance = account_balance
+
+
+    def deposit(self, amount):
+        self.account_balance += amount
+
+    def withdraw(self, amount):
+        if amount > self.account_balance:
+            raise ValueError("Insufficient funds")
+        self.account_balance -= amount
+
+    def display_balance(self):
+        return self.account_balance
