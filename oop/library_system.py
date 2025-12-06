6c93a13456afc54bec69612fe29720e3fc58d9ab
Emmanuel Samson <samsonemmanuel9025@gmail.com>
2025-12-06 14:37:51 +0100

    library system added

---
+++ oop/library_system.py
@@ -0,0 +1,40 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+class EBook(Book):
+    def __init__(self, title, author, file_size):
+        super().__init__(title, author)
+        self.file_size = file_size  # in MB
+
+    def __str__(self):
+        return f'EBook: {self.title} by {self.author}, Size: {self.file_size}MB'
+
+    def __repr__(self):
+        return f"EBook('{self.title}', '{self.author}', {self.file_size})"
+    
+
+class PrintedBook(Book):
+    def __init__(self, title, author, page_count):
+        super().__init__(title, author)
+        self.page_count = page_count  # in grams
+
+    def __str__(self):
+        return f'Printed Book: {self.title} by {self.author}, page_count: {self.page_count}g'
+
+    def __repr__(self):
+        return f"PrintedBook('{self.title}', '{self.author}', {self.page_count})"
+    
+
+class Library:
+    def __init__(self):
+        self.books = []
+
+    def add_book(self, book):
+        self.books.append(book)
+        print(f'Added: {book}')
+
+    def list_books(self):
+        for book in self.books:
+            print(book)
