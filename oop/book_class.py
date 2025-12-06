3480b81210c37679f09f7d1151109a167af3ebdc
Emmanuel Samson <samsonemmanuel9025@gmail.com>
2025-12-06 14:23:34 +0100

    updated

--- oop/book_class.py
+++ oop/book_class.py
@@ -14,4 +14,4 @@ class Book:
     
 
     def __repr__(self):
-        return f"{self.title} by {self.author}, published in {self.year}"
+        return f"Book('{self.title}', '{self.author}', {self.year})"
