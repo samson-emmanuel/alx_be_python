12d3858c68f8332a4d1bc4fbcc28c8414ad6a52f
Emmanuel Samson <samsonemmanuel9025@gmail.com>
2025-12-06 14:56:22 +0100

    corrected return texts

--- oop/library_system.py
+++ oop/library_system.py
@@ -6,29 +6,30 @@ class Book:
     def __str__(self):
         return f"Book: {self.title} by {self.author}"
 
+
 class EBook(Book):
     def __init__(self, title, author, file_size):
         super().__init__(title, author)
-        self.file_size = file_size  # in MB
+        self.file_size = file_size  # in KB
 
     def __str__(self):
-        return f'EBook: {self.title} by {self.author}, Size: {self.file_size}KB'
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
 
     def __repr__(self):
         return f"EBook('{self.title}', '{self.author}', {self.file_size})"
-    
+
 
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
         super().__init__(title, author)
-        self.page_count = page_count  # in grams
+        self.page_count = page_count
 
     def __str__(self):
-        return f'Printed Book: {self.title} by {self.author}, page_count: {self.page_count}KB'
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 
     def __repr__(self):
-        return f"PrintedBook('{self.title}', '{self.author}', {self.page_count})"
-    
+        return f"PrintBook('{self.title}', '{self.author}', {self.page_count})"
+
 
 class Library:
     def __init__(self):
@@ -36,7 +37,7 @@ class Library:
 
     def add_book(self, book):
         self.books.append(book)
-        print(f'Added: {book}')
+        print(f"Added: {book}")
 
     def list_books(self):
         for book in self.books:
